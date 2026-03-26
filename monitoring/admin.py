c58868a084fa90bebf733ebf0b2a85c2b339e30d
Melissa Violet <melissavioletvs@gmail.com>
2026-03-26 12:49:24 +0300

    Registered models

--- monitoring/admin.py
+++ monitoring/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Keyword, ContentItem, Flag
 
-# Register your models here.
+@admin.register(Keyword)
+class KeywordAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+@admin.register(ContentItem)
+class ContentItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'source', 'last_updated')
+
+@admin.register(Flag)
+class FlagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'keyword', 'content_item', 'score', 'status')
+    list_filter = ('status',)
