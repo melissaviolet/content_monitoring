c8f02883532635856b0deb730996caf4a42e2994
Melissa Violet <melissavioletvs@gmail.com>
2026-03-26 11:31:17 +0300

    API view to trigger import

--- monitoring/views.py
+++ monitoring/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Keyword
 from .serializers import KeywordSerilaizer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .services.content_loader import load_mock_data
+
 
 class KeywordViewset(viewsets.ModelViewSet):
     queryset = Keyword.objects.all()
     serializer_class = KeywordSerilaizer
+
+@api_view(['POST'])
+def import_content_view(request):
+    load_mock_data()
+    return Response({"message": "Content imported successfully."})
