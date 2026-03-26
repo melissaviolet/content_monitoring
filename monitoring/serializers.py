73e4c730f50e3e60ba4367a684dcd5fcd6bbdb43
Melissa Violet <melissavioletvs@gmail.com>
2026-03-26 11:23:52 +0300

    Content Item serializer

--- monitoring/serializers.py
+++ monitoring/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from .models import Keyword
+from .models import Keyword, ContentItem
 
+#Keyword serializer
 class KeywordSerilaizer(serializers.ModelSerializer):
     class Meta:
         model = Keyword
+        fields = '__all__'
+
+#ContentItem serializer
+class ContentItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ContentItem
         fields = '__all__'
