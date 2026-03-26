e5b82d74240acc5e4764263f1408a7482715f82e
Melissa Violet <melissavioletvs@gmail.com>
2026-03-26 12:13:07 +0300

    review and suppression logic

--- monitoring/views.py
+++ monitoring/views.py
@@ -1,17 +1,32 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Keyword
-from .serializers import KeywordSerilaizer
+from .models import Keyword, Flag
+from .serializers import KeywordSerilaizer, FlagSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .services.content_loader import load_mock_data
+from django.utils import timezone
 
-
+#Keyword viewset
 class KeywordViewset(viewsets.ModelViewSet):
     queryset = Keyword.objects.all()
     serializer_class = KeywordSerilaizer
 
+#For import content mockup data 
 @api_view(['POST'])
 def import_content_view(request):
     load_mock_data()
     return Response({"message": "Content imported successfully."})
+
+#For review and suppression
+class FlagViewSet(viewsets.ModelViewSet):
+    queryset = Flag.objects.all()
+    serializer_class = FlagSerializer
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        
+        # if reviewer marks it
+        if instance.status in ['relevant', 'irrelevant']:
+            instance.reviewed_at = timezone.now()
+            instance.save()
