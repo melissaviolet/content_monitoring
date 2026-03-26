3dadb71d87370c6d17bdcce4c477d5f693dcf4b8
Melissa Violet <melissavioletvs@gmail.com>
2026-03-26 11:34:00 +0300

    Url added

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -1,7 +1,13 @@
 from rest_framework.routers import DefaultRouter
-from .views import KeywordViewset
+from .views import KeywordViewset, import_content_view
+from django.urls import path
+
 
 router = DefaultRouter()
 router.register(r'keywords', KeywordViewset)
 
 urlpatterns = router.urls
+
+urlpatterns += [
+    path('import-content/', import_content_view)
+]
