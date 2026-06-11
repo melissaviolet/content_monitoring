7988e3000b625f0cebbc9b1322137ac957ced6b8
Melissa Violet <melissavioletvs@gmail.com>
2026-06-11 12:20:22 +0300

    Flags url

--- content_monitoring/urls.py
+++ content_monitoring/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from monitoring.views import dashboard_view
+from monitoring.views import dashboard_view, flags_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('monitoring.urls')),
     path('', dashboard_view, name='dashboard'),
+    path('flags/', flags_view, name='flags'),
 ]
